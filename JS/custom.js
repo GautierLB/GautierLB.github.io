868716e56c7c5089b2826c57e46c34e05cfa591a
LE BISSONNAIS Gautier <gautier.lb@gmail.com>
2015-11-27 11:51:34 +0100

    Fixed issue #1 (Partially)
    #1 The scrolling stops at the bottom but the div still blinks when
    returning to start position

--- JS/custom.js
+++ JS/custom.js
@@ -3,6 +3,24 @@ var sticky = new Waypoint.Sticky({
   element: $('.sticky')[0]
 })
 
+$(document).ready(function() {
+
+     $(window).scroll(function() {
+        var scrollVal = $(this).scrollTop();
+	    console.log('Scrollval = ' + scrollVal + ' Doc.height = ' + $(document).height() + ' Window.height = ' + $(window).height());
+	    console.log('Scrollval = ' + ($(document).height() - $(window).height() - 500));
+	    if ( scrollVal > ($(document).height() - $(window).height()) - 100) {
+	            $('.sticky').removeClass('stuck'); 
+	            $('.sticky').addClass('hitBottom');            
+	    }
+	    else {
+	        $('.sticky').removeClass('hitBottom'); 
+	       	$('.sticky').addClass('stuck');    
+	    }   
+   });
+
+});
+
 $(function(){
   var hash = window.location.hash;
   hash && $('ul.nav a[href="' + hash + '"]').tab('show');
